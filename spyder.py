bbbf5aa9f9d51fcdb597e8018eecc01d99425eff
leon332157 <leon332157@sina.com>
2018-03-09 00:13:41 -0500

    奸笑

--- spyder.py
+++ spyder.py
@@ -2,7 +2,6 @@
 import os
 from bs4 import BeautifulSoup
 import requests
-
 BASE_URL = 'http://www.shangdejy.com'  # 初始网址
 content = requests.get(BASE_URL + '/SiteFiles/Inner/page.aspx?s=1').content.decode('utf8')  # 主页
 bs = BeautifulSoup(content, 'html.parser')  # 实例化bs类
